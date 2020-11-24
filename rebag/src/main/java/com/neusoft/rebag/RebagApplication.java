aa521960caa2d54da3070f8aab0216db5b039496
SZQ0118 <szq_zjg@163.com>
2020-11-24 14:58:18 +0800

    111

---
+++ rebag/src/main/java/com/neusoft/rebag/RebagApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.rebag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebagApplication.class, args);
+    }
+
+}
---
+++ rebag/src/test/java/com/neusoft/rebag/RebagApplicationTests.java
@@ -0,0 +1,10 @@
+package com.neusoft.rebag;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebagApplicationTests {
+
+
+}
