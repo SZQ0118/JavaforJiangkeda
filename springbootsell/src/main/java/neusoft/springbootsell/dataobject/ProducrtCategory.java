b51f808a607ddbe114768524adb323778a9acf00
SZQ0118 <szq_zjg@163.com>
2020-11-26 14:27:05 +0800

    entity modify

--- springbootsell/src/main/java/neusoft/springbootsell/dataobject/ProducrtCategory.java
+++ springbootsell/src/main/java/neusoft/springbootsell/dataobject/ProducrtCategory.java
@@ -1,7 +1,41 @@
 package neusoft.springbootsell.dataobject;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
 /**
  * 商品类目
  */
+@Entity
+@NoArgsConstructor
+@Data
+@DynamicUpdate
 public class ProducrtCategory {
+    /**类目Id*/
+    @Id
+    @GeneratedValue
+    private Integer categoryId;
+
+    /**类目名字*/
+    private String categoryName;
+
+    /**类目编号*/
+    private Integer categoryType;
+
+    /**创建时间*/
+    private Date createTime;
+
+    /**更新时间*/
+    private Date updateTime;
+
+    public ProducrtCategory(String categoryName, Integer categoryType) {
+        this.categoryName = categoryName;
+        this.categoryType = categoryType;
+    }
 }
--- springbootsell/src/main/java/neusoft/springbootsell/dataobject/ProductInfo.java
+++ springbootsell/src/main/java/neusoft/springbootsell/dataobject/ProductInfo.java
@@ -1,7 +1,43 @@
 package neusoft.springbootsell.dataobject;
 
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  * 商品详情
  */
+@Entity
+@Data
+@DynamicUpdate
 public class ProductInfo {
+    @Id
+    @GeneratedValue
+    /**商品Id*/
+    private String ProductId;
+
+    /**商品名字Id*/
+    private String productName;
+    /**商品单价*/
+    private BigDecimal productPrice;
+    /**库存*/
+    private Integer productStock;
+    /**描述*/
+    private String productDecription;
+    /**小图*/
+    private String productIcon;
+    /**状态 0正常 1下架 */
+    private Integer productStatus = 0;
+
+    /**创建时间*/
+    private Date createTime;
+
+    /**更新时间*/
+    private Date updateTime;
+
 }
